13307d916a496d719c96b57f486f1cef73f829b8
Hugo Pires <hugo.manito@gmail.com>
2020-10-18 14:03:18 +0100

    Filtro

--- app.py
+++ app.py
@@ -30,11 +30,13 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+hour_to_filter = st.slider('hour', 0, 23, 17)
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour == hour_to_filter, bins=24, range=(0, 24))[0]
 st.bar_chart(hist_values)
 
 # min: 0h, max: 23h, default: 17h
-hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
